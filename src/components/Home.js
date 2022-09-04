c4cb9ba4ff42a3d9ab4e2c7176d94ab8ff1c25a5
Eduardo Leal <eduardo.leal@MACSPINH7N04F.local>
2022-09-03 22:08:59 -0500

    feat: added new home with input and button to scan barcode

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,92 +1,43 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useEffect, useState} from 'react';
-import {
-  ActivityIndicator,
-  Alert,
-  Button,
-  ScrollView,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
+import React, {useState} from 'react';
+import {Alert, Button, Text, TextInput, View} from 'react-native';
 
 const Home = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [packingId, setPackingId] = useState('');
-
-  useEffect(() => {
-    setLoading(true);
-    fetch('http://192.168.1.71:3000/api/consult-report')
-      .then(res => res.json())
-      .then(res => setData(res))
-      .catch(err => {
-        console.error(err);
-        setError(err);
-      })
-      .finally(() => setLoading(false));
-  }, []);
 
   const handleSearch = () => {
     if (packingId.length > 0) {
-      if (data.length > 0) {
-        const filter = data.filter(d => d.packingdiskno.includes(packingId));
-        setData(filter);
-      } else {
-        Alert.alert('No hay datos');
-      }
     } else {
       Alert.alert('Error', 'Ingrese un numero de packing');
     }
   };
 
-  if (error) {
-    return (
-      <View>
-        <Text>Existe un error</Text>
-      </View>
-    );
-  }
+  const handleSearchBarcode = () => {};
 
   return (
-    <View>
-      {loading ? (
-        <ActivityIndicator />
-      ) : (
-        <View style={{margin: 10}}>
-          <View
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              flexDirection: 'row',
-              marginVertical: 10,
-            }}>
-            <TextInput
-              placeholder="Packing ID"
-              style={{flex: 1, borderWidth: 0.5}}
-              onChangeText={text => setPackingId(text)}
-            />
-            <Button title="Buscar" onPress={handleSearch} />
-          </View>
-          <ScrollView contentContainerStyle={{paddingBottom: 200}}>
-            {data.map((item, index) => {
-              return (
-                <View key={index} style={{padding: 10}}>
-                  <Text>Part Number: {item.partnumber}</Text>
-                  <Text>Build Sequence: {item.buildsequence}</Text>
-                  <Text>Balloon Number: {item.balloonnumber ?? '-'}</Text>
-                  <Text>QTY: {item.qty}</Text>
-                  <Text>PONo: {item.pono}</Text>
-                  <Text>Vender No: {item.vendorno}</Text>
-                  <Text>Packing ID: {item.packingdiskno}</Text>
-                  <Text>Linea: {item.linea}</Text>
-                </View>
-              );
-            })}
-          </ScrollView>
-        </View>
-      )}
+    <View
+      style={{
+        padding: 10,
+        width: '100%',
+        height: '100%',
+        flexGrow: 1,
+        justifyContent: 'center',
+      }}>
+      <View>
+        <Text style={{fontSize: 15, fontWeight: 'bold', marginVertical: 10}}>
+          TMP Picking System
+        </Text>
+        <TextInput
+          placeholder="Packing ID"
+          style={{borderWidth: 1, borderColor: '#bdbdbd', marginVertical: 10}}
+          onChangeText={text => setPackingId(text)}
+        />
+        <Button title="Buscar" onPress={handleSearch} />
+        <Text style={{textAlign: 'center', marginVertical: 10, fontSize: 15}}>
+          o
+        </Text>
+        <Button title="Escanear código" onPress={handleSearchBarcode} />
+      </View>
     </View>
   );
 };
